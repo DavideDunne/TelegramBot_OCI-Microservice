0929c2a0c69eb1431a74f5c00df3ac36ab8eb6f2
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2024-06-05 19:05:20 -0600

    add test controllers template

---
+++ MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/controller/ToDoItemBotControllerTest.java
@@ -0,0 +1,29 @@
+package com.springboot.MyTodoList.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ToDoItemBotControllerTest {
+
+    @Test
+    void getAllToDoItems() {
+    }
+
+    @Test
+    void getToDoItemById() {
+    }
+
+    @Test
+    void addToDoItem() {
+    }
+
+    @Test
+    void updateToDoItem() {
+    }
+
+    @Test
+    void deleteToDoItem() {
+    }
+}
---
+++ MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/controller/ToDoItemControllerTest.java
@@ -0,0 +1,28 @@
+package com.springboot.MyTodoList.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoItemControllerTest {
+
+    @Test
+    void getAllToDoItems() {
+    }
+
+    @Test
+    void getToDoItemById() {
+    }
+
+    @Test
+    void addToDoItem() {
+    }
+
+    @Test
+    void updateToDoItem() {
+    }
+
+    @Test
+    void deleteToDoItem() {
+    }
+}
