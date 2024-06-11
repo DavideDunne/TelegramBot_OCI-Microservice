8d7e76c41fa798766f81b61dcef41ac65123cc37
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2024-06-11 12:23:04 -0600

    create .sql for creating tables and make db-setup.sh execute it

---
+++ MtdrSpring/utils/create-tables.sql
@@ -0,0 +1,40 @@
+-- Create tables for the database
+-- Executed by db-setup.sh
+-- Written in Oracle PL/SQL
+
+CREATE TABLE TODOUSER.usuario (
+    id NUMBER GENERATED ALWAYS AS IDENTITY,
+    nombre1 VARCHAR2(50) NOT NULL,
+    nombre2 VARCHAR2(50),
+    apellido1 VARCHAR2(50) NOT NULL,
+    apellido2 VARCHAR2(50),
+    is_manager NUMBER(1,0) NOT NULL,
+    telegram_username VARCHAR2(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE TODOUSER.tarea (
+    id NUMBER GENERATED ALWAYS AS IDENTITY,
+    nombre VARCHAR2(50) NOT NULL,
+    descripcion VARCHAR2(4000) NOT NULL,
+    id_usuario NUMBER NOT NULL,
+    completado NUMBER(1,0) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_usuario) REFERENCES TODOUSER.usuario(id)
+);
+
+CREATE TABLE TODOUSER.equipo (
+    id NUMBER GENERATED ALWAYS AS IDENTITY,
+    nombre VARCHAR2(50) NOT NULL,
+    id_manager NUMBER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_manager) REFERENCES TODOUSER.usuario(id)
+);
+
+CREATE TABLE TODOUSER.equipo_usuario (
+    id_equipo NUMBER NOT NULL,
+    id_usuario NUMBER NOT NULL,
+    PRIMARY KEY (id_equipo, id_usuario),
+    FOREIGN KEY (id_equipo) REFERENCES TODOUSER.equipo(id),
+    FOREIGN KEY (id_usuario) REFERENCES TODOUSER.usuario(id)
+);
